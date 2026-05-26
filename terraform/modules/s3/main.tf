e3f573d58d0e17f608e32c4ca8a6c49cd95f0bd4
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-26 21:44:29 +0530

    changed s3 bucket name

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "data_lake" {
-  bucket = "${var.project_name}-data-${var.environment}"
+  bucket = "${var.project_name}-datalake-${var.environment}"
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
