51c021bf8f11c3abaade8898ce82fbf092a0c36b
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-26 19:47:58 +0530

    added terraform dev environment code

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,29 @@
+module "s3" {
+    source "../../modules/s3"
+
+    project_name = var.project_name
+    environment = var.environment
+}
+
+module "security_group" {
+    source = "../../modules/security_group"
+
+    project_name = var.project_name
+}
+
+module "iam" {
+    source = "../../modules/iam"
+
+    project_name = var.project_name
+}
+
+module "ec2" {
+    source = "../../modules/ec2"
+
+    project_name = var.project_name
+    environment = var.environment
+    instance_type = var.instance_type
+    key_name = var.key_name
+    security_group_id = module.security_group.security_group_id
+    instance_profile_name = module.iam.instance_profile_name
+}
---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  default = "smartcity360"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "instance_type" {
+  default = "t3.large"
+}
+
+variable "key_name" {}
