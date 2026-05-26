e86714534b248889c9b88288a67eb8f8dc3793e6
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-26 19:48:09 +0530

    added ec2 code

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["585008079281"]
+
+  filter {
+    name   = "name"
+
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+    ]
+  }
+}
+
+resource "aws_instance" "streaming_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    var.security_group_id
+  ]
+
+  iam_instance_profile = var.instance_profile_name
+
+  user_data = file("${path.module}/../../scripts/user_data.sh")
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.project_name}-streaming-server"
+  }
+}
---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "project_name" {}
+variable "environment" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "security_group_id" {}
+variable "instance_profile_name" {}
